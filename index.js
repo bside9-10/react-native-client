3c7a8eceac0ac1d75d74b56b516ec0425c863b27
jooy311 <jooy311@naver.com>
2022-03-15 01:13:10 +0900

    login 기능 추가 #!

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+//import App from './App';
+import App from './src/components/Main';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
