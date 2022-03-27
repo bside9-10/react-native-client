a587a9c73461306100d39355bc5340f4083d59c9
SnowIsWhite <jibae617@gmail.com>
2022-03-27 17:20:30 +0900

    Simple onboarding

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import React from 'react';
 import LoginScreen from './src/views/login/LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnboardingScreens from './src/views/onboarding/Onboarding';
 
 const Stack = createNativeStackNavigator();
 
@@ -20,6 +21,7 @@ const App = () => {
   return (
     <NavigationContainer>{
       <Stack.Navigator>
+        <Stack.Screen name="Onboarding" component={OnboardingScreens} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
       </Stack.Navigator>}</NavigationContainer>
   );
