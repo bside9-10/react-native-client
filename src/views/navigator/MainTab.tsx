7949adf52a1dff3baf73135a47df198b4aa59065
jooy311 <jooy311@naver.com>
2022-05-01 22:14:57 +0900

    Style : tabNavigation 아이콘 추가

--- src/views/navigator/MainTab.tsx
+++ src/views/navigator/MainTab.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text } from "react-native-svg";
+import { Image, View, Text } from "react-native";
 import TodayLaffleScreen from '../TodayLaffleScreen';
 
 const Tab = createBottomTabNavigator();
 
 const ListScreen = () => {
-    return <Text>마이페이지</Text>
+    return <Text>오늘라플</Text>
 }
 
 const TodayPlanScreen = () => { 
@@ -18,12 +18,66 @@ const MyPageScreen = () => {
     return <Text>마이페이지</Text>
 }
 
+const listIcon = ( focused  : boolean) => { 
+    return (
+        // eslint-disable-next-line global-require
+        <Image source={focused ? require('../../../assets/images/laffleListOn.png') : require('../../../assets/images/laffleListOff.png')}
+               resizeMode="contain"
+                           
+        />
+    )
+}
+
+const todayLaffleIcon = ( focused  : boolean) => { 
+    return (
+        // eslint-disable-next-line global-require
+        <Image source={focused ? require('../../../assets/images/todayLaffleOn.png') : require('../../../assets/images/todayLaffleOff.png')}
+               resizeMode="contain"
+                           
+        />
+    )
+}
+
+const mypageIcon = ( focused  : boolean) => { 
+    return (
+        // eslint-disable-next-line global-require
+        <Image source={focused ? require('../../../assets/images/mypageOn.png') : require('../../../assets/images/mypageOff.png')}
+               resizeMode="contain"
+                           
+        />
+    )
+}
+
 const MainTab = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="라플리스트" component={ListScreen}  options={{headerShown:false}}/>
-            <Tab.Screen name="오늘라플" component={TodayPlanScreen} options={{headerShown:false}}/>
-            <Tab.Screen name="마이페이지" component={MyPageScreen}/>
+        <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
+            <Tab.Screen name="라플리스트" component={ListScreen} options={{
+                headerShown: false,
+                // eslint-disable-next-line react/no-unstable-nested-components
+                tabBarIcon: ({ focused }) => (
+                    <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+                        {listIcon(focused)}
+                    </View>
+                )
+            }} />
+            <Tab.Screen name="오늘라플" component={TodayPlanScreen} options={{
+                headerShown: false,
+                // eslint-disable-next-line react/no-unstable-nested-components
+                tabBarIcon: ({ focused }) => (
+                    <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+                        {todayLaffleIcon(focused)}
+                    </View>
+                )
+            }} />
+            <Tab.Screen name="마이페이지" component={MyPageScreen} options={{
+                headerShown: false,
+                // eslint-disable-next-line react/no-unstable-nested-components
+                tabBarIcon: ({ focused }) => (
+                    <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+                        {mypageIcon(focused)}
+                    </View>
+                )
+            }} />
         </Tab.Navigator>
     );
 }
