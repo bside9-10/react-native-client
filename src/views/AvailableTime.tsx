84c51e2fe581c4826aed8e34ecdedcdc9361ae14
jooy311 <jooy311@naver.com>
2022-05-13 21:14:24 +0900

    feat : 사용가능시간 디자인 추가

---
+++ src/views/AvailableTime.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from "react-native";
+
+const AvailableTime = () => { 
+    return (
+        <Text>df</Text>
+    );
+}
+
+export default AvailableTime;
