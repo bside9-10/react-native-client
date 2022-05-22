b9130b06ca864b372e0e6442f58286fda5a013ad
Sodam Shin <damsoshin@gmail.com>
2022-05-22 19:53:34 +0900

    ⚡️ 오늘의 라플 API 연동 #27

--- src/views/TodayLaffleScreen.tsx
+++ src/views/TodayLaffleScreen.tsx
@@ -1,41 +1,46 @@
 import React from 'react';
-import { StyleSheet, View, SafeAreaView, ScrollView, Dimensions } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
+import {useQuery} from 'react-query';
 import CustomCheckbox from '../components/goal/CustomCheckBox';
+import getTodayLaffle from '../helper/api/TodayLaffleAPI';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-const TodayLaffleScreen = () => { 
-    // console.log(windowHeight);
-    
-    return (
-        <SafeAreaView>
-          <View>                
-            <ScrollView style={styles.scrollView}>
-              <CustomCheckbox toDos="오늘 뭐하지" date="매주금요일" />  
-              <CustomCheckbox toDos="오늘 뭐하지" date="매주금요일" />  
-              <CustomCheckbox toDos="오늘 뭐하지" date="매주금요일" />  
-              <CustomCheckbox toDos="오늘 뭐하지" date="매주금요일" />  
-              <CustomCheckbox toDos="오늘 뭐하지" date="매주금요일" /> 
-              <CustomCheckbox toDos="오늘 뭐하지" date="매주금요일" /> 
-              <CustomCheckbox toDos="오늘 뭐하지" date="매주금요일" /> 
-              <CustomCheckbox toDos="오늘 뭐하지" date="매주금요일" /> 
-              <CustomCheckbox toDos="오늘 뭐하지" date="매주금요일" /> 
-            </ScrollView>
-          </View>
-        </SafeAreaView>
-    );
+const TodayLaffleScreen = () => {
+  // console.log(windowHeight);
+
+  const resultQuery = useQuery('getTodayLaffle', getTodayLaffle);
+
+  const {data} = resultQuery;
+  return (
+    <SafeAreaView>
+      <View>
+        <ScrollView style={styles.scrollView}>
+          {data?.data.response.map((v, i) => {
+            return <CustomCheckbox key={i} categoryInfo={v} />;
+          })}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default TodayLaffleScreen;
 
 const styles = StyleSheet.create({
   scrollView: {
-    top: (windowHeight / 4.72) ,
-    height: (windowHeight),
-    backgroundColor: "white",
+    top: windowHeight / 4.72,
+    height: windowHeight,
+    backgroundColor: 'white',
     borderTopLeftRadius: 45,
     borderTopRightRadius: 45,
     // marginHorizontal: 10
-  }
-})
+  },
+});
